bf3f7406fce1bb1896ebb289427363313f20b0ea
Andrés Ávila <andaviaco@gmail.com>
2021-08-28 22:39:08 -0500

    fix model path

--- src/predictor.py
+++ src/predictor.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.externals import joblib
 
-model = joblib.load('../model/model-sk22.joblib')
-scaler = joblib.load('../model/model-scaler-sk22.joblib')
+model = joblib.load('../model/model-sk22-latest.joblib')
+scaler = joblib.load('../model/model-scaler-sk22-latest.joblib')
 
 def predict(raw_values):
   values = (np.array(raw_values)).reshape(1, -1)
